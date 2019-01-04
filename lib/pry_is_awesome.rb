c22d2dac5184e947fb0d0d73fce138894808ec76
Brandon Brasson <rocksong622@gmail.com>
2019-01-04 23:49:46 +0000

    Automatically backed up by Learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -5,7 +5,7 @@ def prying_into_the_method
 	 inside_the_method
 	 "We're about to stop because of pry!"
 	binding.pry
-	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
+	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!"
  this_variable_hasnt_been_interpreted_yet
 end
 
